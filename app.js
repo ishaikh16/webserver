aed651ce10c763cab117e52d44360504c3d401b3
Irshad Shaikh <ishaikh16@yahoo.com>
2013-02-20 16:19:43 +0530

    rectified a bug that did not allow binding to port 4848, added PORT variable

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+
 	var express = require('express');
 	var app = express();
-	app.use(express.static(__dirname + '/public'));
-	app.listen(4848);
+	var port = process.env.PORT || 4848;
+	var.use(express.static(__dirname + '/public'));
+	app.listen(port);
    
