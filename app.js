6b033a814eadecd23688a02aa915322da2584132
Irshad Shaikh <ishaikh16@yahoo.com>
2013-02-20 16:29:28 +0530

    systax error 'var.use' in place of 'app.use' removed

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 	var express = require('express');
 	var app = express();
 	var port = process.env.PORT || 4848;
-	var.use(express.static(__dirname + '/public'));
+	app.use(express.static(__dirname + '/public'));
 	app.listen(port);
    
